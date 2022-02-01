e2b58c5e5375dc983231716a6af289f01ff6ae1e
yedidyar <yedidyarashi@gmail.com>
2022-02-01 12:13:57 +0200

    feat: show the models from the sequelizeUI server

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { View } from "./assets/icons";
+import Icon from "./components/Icon";
+import Item from "./components/Item";
+import { useGetModelsNameQuery } from "./services/sequelize";
+
 const App = () => {
-  return <div className="App"></div>;
+  const { data: models } = useGetModelsNameQuery();
+
+  return (
+    <div className="App">
+      {models?.map((model) => (
+        <div>
+          <Item
+            label={model}
+            Icon={[<Icon Icon={View} onClickHandler={() => {}} />]}
+          ></Item>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
