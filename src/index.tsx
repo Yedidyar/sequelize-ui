50a645b47e31824632f1092adcfd4d2212c5cd82
yedidyar <yedidyarashi@gmail.com>
2022-02-02 19:11:32 +0200

    ci(dependencies): add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
@@ -9,7 +10,9 @@ import "./index.css";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
